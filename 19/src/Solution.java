2549d8607b2ac222fd5557ff939c6adefd2ab429
161250005 <1046277348@qq.com>
2020-10-18 11:32:53 +0800

    19:删除链表的倒数第N个节点

---
+++ 19/src/Solution.java
@@ -0,0 +1,24 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(0,head);
+        ListNode fast = head;
+        ListNode slow = dummyHead;
+        for(int i=1;i<n;i++){
+            fast = fast.next;
+        }
+        while (fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummyHead.next;
+    }
+}
